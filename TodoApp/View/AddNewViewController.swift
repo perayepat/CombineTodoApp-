627ed6033b794fa6b770b9eddb7ec40aab26342d
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-03-26 19:05:47 +0200

    Data flow from adding a new item to refreshing the view is completed

---
+++ TodoApp/View/AddNewViewController.swift
@@ -0,0 +1,41 @@
+import UIKit
+import Combine
+
+class AddNewViewController: UIViewController {
+
+    // get reference to viewmodel
+    var taskListModel: TaskListModel?
+    var text: String = ""
+    
+    var subscriptions = Set<AnyCancellable>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textField.addTarget(self, action: #selector(updateText), for: .editingChanged)
+    }
+    
+    @objc func updateText() {
+        self.text = textField.text ?? ""
+    }
+    
+    
+    // MARK: - outlets
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func doneButtonPressed(_ sender: UIButton) {
+        // create new task from textfield
+        taskListModel?.addNewTask.send(text)
+        dismiss()
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIButton) {
+        dismiss()
+    }
+    
+    func dismiss() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ TodoApp/View/TodoListViewController.swift
@@ -0,0 +1,46 @@
+import UIKit
+import Combine
+
+class TodoListViewController: UIViewController {
+
+    // use viewmodel to populate table
+    let taskViewModel  = TaskListModel()
+    @IBOutlet weak var tableView: UITableView!
+    
+    var subscriptions = Set<AnyCancellable>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        
+        // add data stream that calls tableView.reloadData() when data changes
+        taskViewModel.tasks.sink { [unowned self] _ in
+         /// Whenever the tasks are sent down stream the view should update
+            self.tableView.reloadData()
+        }
+        .store(in: &subscriptions)
+    }
+
+    @IBSegueAction func addNewViewIsGoingToAppear(_ coder: NSCoder) -> AddNewViewController? {
+        let controller = AddNewViewController(coder: coder)
+        // hande over viewmodel to controller
+        controller?.taskListModel = taskViewModel
+        return controller
+    }
+    
+}
+
+extension TodoListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskViewModel.tasks.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        cell.textLabel?.text = taskViewModel.tasks.value[indexPath.row]
+        return cell
+    }
+    
+    
+}
